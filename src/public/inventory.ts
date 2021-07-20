abdb60999657c9b1023f64070f0848c93814d121
Amir-Benkoula <amir.benkoula@jwalab.com>
2021-07-20 08:39:07 +0200

    BSP-109: Transfer Inventory Item
    - Add Transfer public and private handlers
    - Add Transfer docs
    - Add Transfer tests

--- src/public/inventory.ts
+++ src/public/inventory.ts
@@ -1,7 +1,17 @@
-import { JSONInventoryItem } from "@jwalab/tokenization-service-contracts";
 import { Subscription } from "nats";
 
 import { getConnection, jsonCodec, PublicNatsHandler } from "../services/nats";
+import { inventoryIdValidator } from "../utils/validators";
+
+interface UserResponse {
+    inventory_address: string;
+}
+
+interface GetInventoryItemResponse {
+    item_id: number;
+    user_id: number;
+    instance_number: number;
+}
 
 export const inventoryPublicHandlers: PublicNatsHandler[] = [
     [
@@ -10,42 +20,47 @@ export const inventoryPublicHandlers: PublicNatsHandler[] = [
         async (subscription: Subscription): Promise<void> => {
             for await (const message of subscription) {
                 const natsConnection = getConnection();
-                const urlParameter = String(message.subject).split(".")[2];
+                const inventory_item_id = String(message.subject).split(".")[2];
 
                 try {
-                    const itemStoreGetInventoryItemResponse = await natsConnection.request(
-                        "item-store.get_inventory_item",
-                        jsonCodec.encode({
-                            inventory_item_id: urlParameter
-                        })
-                    );
+                    await inventoryIdValidator.validate(inventory_item_id);
 
-                    const inventoryItem = jsonCodec.decode(
-                        itemStoreGetInventoryItemResponse.data
-                    ) as JSONInventoryItem;
+                    const itemStoreGetInventoryItemResponse =
+                        await natsConnection.request(
+                            "item-store.get_inventory_item",
+                            jsonCodec.encode({
+                                inventory_item_id
+                            })
+                        );
+
+                    const { user_id, instance_number, item_id } =
+                        jsonCodec.decode(
+                            itemStoreGetInventoryItemResponse.data
+                        ) as GetInventoryItemResponse;
 
                     const getUserResponse = await natsConnection.request(
                         "item-store.get_user",
                         jsonCodec.encode({
-                            user_id: inventoryItem.user_id
+                            user_id
                         })
                     );
 
-                    const user = jsonCodec.decode(getUserResponse.data) as {
-                        inventory_address: string;
-                    };
+                    const { inventory_address } = jsonCodec.decode(
+                        getUserResponse.data
+                    ) as UserResponse;
 
-                    const tokenizationServiceGetInventoryItemReponse = await natsConnection.request(
-                        "tokenization-service.get_inventory_item",
-                        jsonCodec.encode({
-                            inventory_address: user.inventory_address,
-                            item_id: inventoryItem.item_id,
-                            instance_number: inventoryItem.instance_number
-                        })
-                    );
+                    const tokenizationServiceGetInventoryItemResponse =
+                        await natsConnection.request(
+                            "tokenization-service.get_inventory_item",
+                            jsonCodec.encode({
+                                inventory_address,
+                                item_id,
+                                instance_number
+                            })
+                        );
 
                     message.respond(
-                        tokenizationServiceGetInventoryItemReponse.data
+                        tokenizationServiceGetInventoryItemResponse.data
                     );
                 } catch (err) {
                     console.error(err);
